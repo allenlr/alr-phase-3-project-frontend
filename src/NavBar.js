92a76c7f7e56006836d2f8c5083139a12935fee1
allenlr <allenlru55@gmail.com>
2024-01-22 03:23:30 -0600

    add further styling to NavBar links

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,13 +3,22 @@ import { NavLink } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import Box from '@mui/material/Box';
+
+
 
 function NavBar(){
 
     return (
         <AppBar position="static" color="customColor">
             <Toolbar>
-                <Button color="white" style={{ marginRight: 'auto', fontWeight: 'bold', fontSize: '1rem'}} component={NavLink} to='/'>Home</Button>
+                <Button color="white" style={{ marginRight: 'auto', fontWeight: 'bold', fontSize: '1rem'}} component={NavLink} to='/' >
+                    <Box display="flex" alignItems="center">
+                        <HomeIcon style={{ marginBottom: 4, marginRight: 8 }} /> 
+                        <span>Home</span>
+                    </Box>
+                </Button>
                 <Button color="white" component={NavLink} to="/users" style={{ fontWeight: 'bold' }}>
                     Users
                 </Button>
